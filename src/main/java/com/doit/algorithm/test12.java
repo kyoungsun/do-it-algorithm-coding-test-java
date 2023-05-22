539bbfb7d8e830a50861c5f48c5f03baadd1616f
Jake-torder <pjk1@torder.co.kr>
2023-05-23 00:19:25 +0900

    12

---
+++ src/main/java/com/doit/algorithm/test12.java
@@ -0,0 +1,44 @@
+package com.doit.algorithm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class test12 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(bf.readLine());
+		int[] A = new int[n];
+		int[] ans = new int[n];
+		String[] str = bf.readLine().split(" ");
+		for(int i=0; i<n; i++) {
+			A[i] = Integer.parseInt(str[i]);
+		}
+		Stack<Integer> myStack = new Stack<>();
+		myStack.push(0);
+		for(int i=0; i<n; i++) {
+			while(!myStack.isEmpty() && A[myStack.peek()] < A[i]) {
+				ans[myStack.pop()] = A[i];
+			}
+			myStack.push(i);
+		}
+		while(!myStack.empty()) {
+			ans[myStack.pop()] = -1;
+		}
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for(int i=0; i<n; i++) {
+			bw.write(ans[i]+" ");
+		}
+		bw.write("\n");
+		bw.flush();
+		
+	}
+}
